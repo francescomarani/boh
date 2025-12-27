2e3c4254c4ce6aa87f8de2234548abfe66db56eb
SimoneUtili <simoutili@gmail.com>
2025-12-27 19:32:04 +0100

    Delete build directory

--- albert_example/build/lib/urdfenvs/generic_mujoco/__init__.py
+++
@@ -1,6 +0,0 @@
-from gymnasium import register
-from urdfenvs.generic_mujoco.generic_mujoco_env import GenericMujocoEnv
-register(
-    id='generic-mujoco-env-v0',
-    entry_point='urdfenvs.generic_mujoco.generic_mujoco_env:GenericMujocoEnv'
-)
